e24ae28afe634597e29050d20aee24dc6ff361cb
tangos974 <fremonttanguy@yahoo.fr>
2023-09-28 19:36:24 +0200

    Split data

--- irisClassification.py
+++ irisClassification.py
@@ -1,4 +1,6 @@
+import numpy as np
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 
 iris_dataset = load_iris()
 #this function returns a bunch object, similar to a python dict
@@ -17,6 +19,21 @@ iris_dataset = load_iris()
 """
 #Data is numpy array format (150, 4)
 print(type(iris_dataset['data']))
-print(iris_dataset['data'].shape)
+#print(iris_dataset['data'].shape)
+#print(iris_dataset['target'].shape)
+print(iris_dataset['data'])
+print(iris_dataset['target'])
 """
+#print(iris_dataset['data'][:5])
 
+#scikit-learn has train_test_split function that
+#automatically spearates data into two arrays
+
+X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0)
+
+"""
+print(X_train.shape)
+print(X_test.shape)
+print(y_train.shape)
+print(y_test.shape)
+"""
